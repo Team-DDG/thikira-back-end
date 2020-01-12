4f0c57a70dbfc7ab70d4041aa62a03bdbd248986
JepangLee <ljsung0805@gmail.com>
2020-01-12 15:52:40 +0900

    Update merge TokenModule into UtilModule

--- libs/util/src/class/token.class.ts
+++ libs/util/src/class/token.class.ts
@@ -1,6 +1,6 @@
 export class Token {
-  public readonly accessToken: string;
-  public readonly refreshToken: string;
+  public readonly accessToken?: string;
+  public readonly refreshToken?: string;
 
   constructor(token) {
     Object.assign(this, token);
