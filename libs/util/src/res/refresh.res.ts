fd42b90189071b83dbed5dc94b9abaad75aebb0c
JepangLee <ljsung0805@gmail.com>
2020-01-12 19:25:50 +0900

    Update UtilModule

--- libs/util/src/res/refresh.res.ts
+++ libs/util/src/res/refresh.res.ts
@@ -4,9 +4,6 @@ export class ResRefresh {
   @ApiProperty()
   public readonly accessToken: string;
 
-  @ApiProperty()
-  public readonly refreshToken: string;
-
   constructor(token) {
     Object.assign(this, token);
   }
--- libs/util/src/res/sign_in.res.ts
+++ libs/util/src/res/sign_in.res.ts
@@ -4,6 +4,9 @@ export class ResSignIn {
   @ApiProperty()
   public readonly accessToken: string;
 
+  @ApiProperty()
+  public readonly refreshToken: string;
+
   constructor(token) {
     Object.assign(this, token);
   }
