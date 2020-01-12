7af46b942c89c856b9bafe094fe65510676d402a
JepangLee <ljsung0805@gmail.com>
2020-01-12 15:31:57 +0900

    Test dependency of RestaurantServiceSpec

--- libs/restaurant/src/restaurant.service.spec.ts
+++ libs/restaurant/src/restaurant.service.spec.ts
@@ -1,3 +1,6 @@
+import { MongoModule } from '@app/mongo';
+import { TokenModule } from '@app/token';
+import { UtilModule } from '@app/util';
 import { Test, TestingModule } from '@nestjs/testing';
 import { RestaurantService } from './restaurant.service';
 
@@ -6,6 +9,7 @@ describe('RestaurantService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [MongoModule, UtilModule, TokenModule],
       providers: [RestaurantService],
     }).compile();
 
