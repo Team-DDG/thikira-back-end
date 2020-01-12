96e7ebdf3dadee24d3f926c48e92731fce9d3f82
JepangLee <ljsung0805@gmail.com>
2020-01-12 16:26:06 +0900

    Test MongoClient closing of RestaurantServiceSpec

--- libs/restaurant/src/restaurant.service.spec.ts
+++ libs/restaurant/src/restaurant.service.spec.ts
@@ -1,18 +1,24 @@
-import { MongoModule } from '@app/mongo';
+import { MongoModule, MongoService } from '@app/mongo';
 import { UtilModule } from '@app/util';
 import { Test, TestingModule } from '@nestjs/testing';
 import { RestaurantService } from './restaurant.service';
 
 describe('RestaurantService', () => {
   let service: RestaurantService;
+  let mongo: MongoService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [MongoModule, UtilModule],
       providers: [RestaurantService],
     }).compile();
 
     service = module.get<RestaurantService>(RestaurantService);
+    mongo = module.get<MongoService>(MongoService);
+  });
+
+  afterAll(async () => {
+    await mongo.close();
   });
 
   it('should be defined', () => {
