7c013e4cde1c91bd22a4e5d351756b573c451e51
JepangLee <ljsung0805@gmail.com>
2020-02-07 05:00:13 +0900

    Update dependency

--- libs/menu/src/menu.module.ts
+++ libs/menu/src/menu.module.ts
@@ -1,7 +1,9 @@
+import { UtilModule } from '@app/util';
 import { Module } from '@nestjs/common';
 import { MenuService } from './menu.service';
 
 @Module({
+  imports: [UtilModule],
   providers: [MenuService],
   exports: [MenuService],
 })
