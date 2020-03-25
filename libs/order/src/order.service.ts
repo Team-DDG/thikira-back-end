0e79e77a5ae9a8b9b724af3598c6db2cca96d917
JepangLee <ljsung0805@gmail.com>
2020-03-25 18:48:10 +0900

    Update unused method

--- libs/order/src/order.service.ts
+++ libs/order/src/order.service.ts
@@ -106,10 +106,6 @@ export class OrderService {
 
   // only use in test
 
-  public async get_order(od_id: string | ObjectID): Promise<Order> {
-    return this.db_service.find_order_by_id(od_id);
-  }
-
   public async remove_order(od_id: ObjectID | string): Promise<void> {
     await this.db_service.delete_order(od_id);
   }
