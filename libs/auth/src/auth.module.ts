108da62242bc5ad1bcc9137746c1d7ad1f90871e
JepangLee <ljsung0805@gmail.com>
2020-05-30 20:59:30 +0900

    Chore: Edit AuthModule

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -2,12 +2,13 @@ import { config } from '@app/config';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
-import { LocalStrategy, JwtStrategy } from './strategies';
 
 @Module({
   exports: [AuthService],
   imports: [JwtModule.register({ secret: config.JWT_SECRET })],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [
+    AuthService, //LocalStrategy, JwtStrategy
+  ],
 })
 export class AuthModule {
 }
