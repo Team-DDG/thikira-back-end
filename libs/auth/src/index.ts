368eb942062e85f836a54ca692a54ea79f11e605
JepangLee <ljsung0805@gmail.com>
2020-05-30 21:01:29 +0900

    Chore: Edit AuthModule

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -2,5 +2,5 @@ export * from './auth.module';
 export * from './auth.service';
 
 export * from './enum';
-export * from './guards';
-export * from './strategies';
+// export * from './guards';
+// export * from './strategies';
